0d5b540a217ad7573b420288ac6b33993d8cfe92
wcaokaze <wcaokaze@gmail.com>
2024-07-21 18:57:59 +0900

    CacheInternalのインスタンス化処理をCachePoolのコンストラクタの引数から削除

--- panoptiqon/src/rust/pool.rs
+++ panoptiqon/src/rust/pool.rs
@@ -21,6 +21,7 @@ use fnv::FnvHashMap;
 use {
    crate::convert_java::ConvertJava,
    jni::JavaVM,
+   jni::JNIEnv,
    jni::objects::GlobalRef
 };
 
@@ -28,7 +29,6 @@ use crate::cache::Cache;
 
 #[cfg(feature="jvm")]
 pub(crate) struct CachePool<K, T: ConvertJava> {
-   jvm_state_instantiation: Box<dyn Fn(&T) -> GlobalRef>,
    jvm: JavaVM,
    map: FnvHashMap<K, Cache<T>>
 }
@@ -43,9 +43,8 @@ impl<K, T> CachePool<K, T>
    where K: Hash + Eq,
          T: ConvertJava
 {
-   pub fn new(jvm_state_instantiation: Box<dyn Fn(&T) -> GlobalRef>, jvm: JavaVM) -> Self {
+   pub fn new(jvm: JavaVM) -> Self {
       CachePool {
-         jvm_state_instantiation,
          jvm,
          map: FnvHashMap::default()
       }
@@ -55,11 +54,26 @@ impl<K, T> CachePool<K, T>
       self.map.entry(key).or_insert_with(|| {
          let initial_value = initial_value();
 
-         let jvm_state_instantiation = &self.jvm_state_instantiation;
-         let jvm_state = jvm_state_instantiation(&initial_value);
+         let mut env = self.jvm.get_env().unwrap();
+         let jvm_state = Self::create_jvm_state(&mut env, &initial_value);
          let jvm = unsafe { JavaVM::from_raw(self.jvm.get_java_vm_pointer()).unwrap() };
+
          Cache::new(jvm_state, jvm, initial_value)
       })
+   }
+
+   fn create_jvm_state(env: &mut JNIEnv, initial_value: &T) -> GlobalRef {
+      let java_initial_value = initial_value.clone_into_java(env);
+
+      let local_object = env
+         .new_object(
+            "com/wcaokaze/probosqis/panoptiqon/CacheInternal",
+            "(java/lang/Object)V",
+            &[(&java_initial_value).into()]
+         )
+         .unwrap();
+
+      env.new_global_ref(local_object).unwrap()
    }
 }
 
@@ -84,40 +98,16 @@ impl<K, T> CachePool<K, T>
 #[cfg(feature="jni-test")]
 mod jni_tests {
    use jni::JNIEnv;
-   use jni::objects::{GlobalRef, JObject};
-
-   use crate::convert_java::ConvertJava;
-   use crate::test_utils::get_vm;
+   use jni::objects::JObject;
 
    use super::CachePool;
-
-   fn instantiate_jvm_state(initial_value: &i32) -> GlobalRef {
-      let vm = get_vm();
-      let mut env = vm.get_env().unwrap();
-
-      let java_initial_value = initial_value.clone_into_java(&mut env);
-
-      let local_object = env
-         .new_object(
-            "com/wcaokaze/probosqis/panoptiqon/CacheInternal",
-            "(java/lang/Object)V",
-            &[(&java_initial_value).into()]
-         )
-         .unwrap();
-
-      env.new_global_ref(local_object).unwrap()
-   }
 
    #[no_mangle]
    extern "C" fn Java_com_wcaokaze_probosqis_panoptiqon_CachePoolTest_createCache(
       env: JNIEnv,
       _obj: JObject
    ) {
-      let jvm_state_instantiation = Box::new(instantiate_jvm_state);
-      let mut pool = CachePool::new(
-         jvm_state_instantiation,
-         env.get_java_vm().unwrap()
-      );
+      let mut pool = CachePool::new(env.get_java_vm().unwrap());
 
       let cache = pool.get("A".to_string(), || 42);
       assert_eq!(42, **cache);
@@ -131,11 +121,7 @@ mod jni_tests {
       env: JNIEnv,
       _obj: JObject
    ) {
-      let jvm_state_instantiation = Box::new(instantiate_jvm_state);
-      let mut pool = CachePool::new(
-         jvm_state_instantiation,
-         env.get_java_vm().unwrap()
-      );
+      let mut pool = CachePool::new(env.get_java_vm().unwrap());
       let cache1_ptr = pool.get("A".to_string(), || 42) as *const _;
       let cache2_ptr = pool.get("A".to_string(), || 42) as *const _;
       let cache3_ptr = pool.get("B".to_string(), || 42) as *const _;
