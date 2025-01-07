f911682ed9b5e0223aef86dcaa0a1df402993d17
wcaokaze <wcaokaze@gmail.com>
2025-01-07 21:26:06 +0900

    Repositoryの型引数Sにデフォルト実型引数として `fn(&T) -> K` を設定

--- panoptiqon/src/rust/repository.rs
+++ panoptiqon/src/rust/repository.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 wcaokaze
+ * Copyright 2024-2025 wcaokaze
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ use {
 use crate::cache::Cache;
 use crate::pool::UniqueCachePool;
 
-pub struct Repository<K, T, S>
+pub struct Repository<K, T, S = fn(&T) -> K>
    where S: Fn(&T) -> K
 {
    pool: UniqueCachePool<K, T>,
