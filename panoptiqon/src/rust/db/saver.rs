2e7b0a95921587916e264ca712e046b11f7ab131
wcaokaze <wcaokaze@gmail.com>
2025-06-20 01:55:38 +0900

    Saver::saveのテストを追加

--- panoptiqon/src/rust/db/saver.rs
+++ panoptiqon/src/rust/db/saver.rs
@@ -41,3 +41,62 @@ impl Saver {
       Ok(())
    }
 }
+
+#[cfg(test)]
+mod test {
+   use super::{DirPath, Saver, Serializer};
+
+   #[test]
+   fn save() {
+      use std::fs;
+      use std::path::{Path, PathBuf};
+      use std::sync::Arc;
+      use scopeguard::defer;
+      use serde::Serialize;
+      use crate::db::savable::Savable;
+      use crate::db::save_task::SaveTask;
+
+      #[derive(Serialize)]
+      struct SavableImpl {
+         id: i32,
+         value: String
+      }
+
+      impl Savable for SavableImpl {
+         fn file_path(&self, dir_path: &Path) -> PathBuf {
+            dir_path.join(self.id.to_string())
+         }
+
+         fn serialize(
+            &self,
+            serializer: Serializer
+         ) -> anyhow::Result<
+            <Serializer as serde::Serializer>::Ok,
+            <Serializer as serde::Serializer>::Error
+         > {
+            Serialize::serialize(self, serializer)
+         }
+      }
+
+      let savable = SavableImpl {
+         id: 0,
+         value: "value".to_string()
+      };
+
+      let dir_path = DirPath::new(PathBuf::from("test/Saver/save"));
+      let task = SaveTask::new(&dir_path, Arc::new(savable));
+      let result = Saver::save(task);
+      assert!(result.is_ok());
+
+      defer! {
+         fs::remove_file("test/Saver/save/0").unwrap()
+      }
+
+      let saved_text = fs::read_to_string("test/Saver/save/0").unwrap();
+
+      assert_eq!(
+         r#"{"id":0,"value":"value"}"#,
+         &saved_text
+      );
+   }
+}
